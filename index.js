4ea260be6da7ab32053e2f419233659d9a3c7b86
Suchitra Naik <susanaik@gmail.com>
2018-07-11 13:14:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function lowerCaseDrivers(drivers){
 function nameToAttributes(drivers){
   return drivers.map(
     function (driver) {
-          return  {firstName: driver.split(' ').first,lastName: driver.split(' ').last}
+          return  driver{firstName: driver.split(' ').first,lastName: driver.split(' ').last}
     }
 );
 }

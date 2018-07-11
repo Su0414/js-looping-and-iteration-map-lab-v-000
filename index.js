007c93fd6918770f276e49ccd81e21d24dbbba00
Suchitra Naik <susanaik@gmail.com>
2018-07-11 12:32:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file.
+drivers={};
+
+function lowerCaseDrivers(drivers){
+  map(drivers, function (driver) { return driver.toLowerCase(); });
+}
+
+function nameToAttributes(drivers){
+  let driverName = drivers.split(',');
+  let name = driverName.split(' ');
+  
+}
+
+function attributesToPhrase(drivers){
+  
+}
